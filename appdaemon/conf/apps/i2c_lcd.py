704c3c1827be12c05ab46717aa8b6655ff21cc2c
Roman Labovsky <roman.labovsky@gmail.com>
2020-04-17 16:45:02 +0200

    Adding of MAX31850K.

--- appdaemon/conf/apps/i2c_lcd.py
+++ appdaemon/conf/apps/i2c_lcd.py
@@ -4,12 +4,12 @@ import i2c_lcd_driver
 class I2CLCD(hass.Hass):
 
   def initialize(self):
-    self.listen_state(self.trigger, "sensor.hot_water_tank_top_temperature")
     self.listen_state(self.trigger, "sensor.hot_water_tank_middle_temperature")
-    self.listen_state(self.trigger, "input_boolean.manual_control_devices")
+    self.listen_state(self.trigger, "sensor.hot_water_tank_bottom_temperature")
 
-  def trigger(self, entity, attribute, old, new, kwargs):
+  def trigger(self, entity, attribute, old, new, kwargs):    
     first_floor_lcd = i2c_lcd_driver.lcd(0x26)
+    second_floor_lcd = i2c_lcd_driver.lcd(0x27)
     degree = [      
         [ 0b00111,
 	      0b00101,
@@ -21,10 +21,14 @@ class I2CLCD(hass.Hass):
 	      0b00000
           ],]
     first_floor_lcd.lcd_load_custom_chars(degree)
+    second_floor_lcd.lcd_load_custom_chars(degree)
  
-    hot_water_tank_top_temperature = self.get_state("sensor.hot_water_tank_top_temperature")
     hot_water_tank_middle_temperature = self.get_state("sensor.hot_water_tank_middle_temperature")
+    hot_water_tank_bottom_temperature = self.get_state("sensor.hot_water_tank_bottom_temperature")
     
-    first_floor_lcd.lcd_display_string("Cidlo 1: "+ hot_water_tank_top_temperature + " " + chr(0) + "C", 1)
-    first_floor_lcd.lcd_display_string("Cidlo 2: "+ hot_water_tank_middle_temperature + " " + chr(0) + "C", 2)
+    first_floor_lcd.lcd_display_string("Cidlo 2: "+ hot_water_tank_middle_temperature + " " + chr(0) + "C", 1)
+    first_floor_lcd.lcd_display_string("Cidlo 3: "+ hot_water_tank_bottom_temperature + " " + chr(0) + "C", 2)
+
+    second_floor_lcd.lcd_display_string("Cidlo 2: "+ hot_water_tank_middle_temperature + " " + chr(0) + "C", 1)
+    second_floor_lcd.lcd_display_string("Cidlo 3: "+ hot_water_tank_bottom_temperature + " " + chr(0) + "C", 2)
    
