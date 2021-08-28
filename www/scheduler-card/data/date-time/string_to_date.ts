2b226f575790100e03f890325d7ed2baf2b1d4dc
Roman Labovský <roman.labovsky@gmail.com>
2021-08-28 17:14:30 +0200

    Remove old files.

--- www/scheduler-card/data/date-time/string_to_date.ts
+++
@@ -1,8 +0,0 @@
-
-
-
-export function stringToDate(timeString: string) {
-  const parts = timeString.split(':').map(Number);
-  const now = new Date();
-  return new Date(now.getFullYear(), now.getMonth(), now.getDate(), ...parts);
-}
